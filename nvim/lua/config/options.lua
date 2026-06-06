4637badc8a88413bee7a8b12ab0e8f451885961b
JuadaDev <juadadev@gmail.com>
2026-06-06 11:35:42 -0500

    ✨ feat(config): add Spanish support to spellchecker for markdown
    Configured 'vim.opt.spelllang' to inclue both Spanish and English.
    This prevents false-positive spellcheck errors (red underlines) when
    writing documentation or notes in Spanish within Markdown files.

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,3 +1,6 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- So that it recognizes the Spanish language when using Markdown, for example, and does not show spelling errors (red underline)
+vim.opt.spelllang = { "es", "en" }
